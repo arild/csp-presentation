a9847e2960068daec1f1acaf611a0b226743a09b
Arild Nilsen <arild.nilsen2@gmail.com>
2013-08-17 13:44:49 +0200

    rename template.html to index.html

--- serve.sh
+++ serve.sh
@@ -19,4 +19,4 @@ else
   open=xdg-open
 fi
 
-$open http://localhost:$port/template.html && python -m SimpleHTTPServer $port;
+$open http://localhost:$port/index.html && python -m SimpleHTTPServer $port;
--- slide_config.js
+++ slide_config.js
@@ -11,7 +11,7 @@ var SLIDE_CONFIG = {
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in index.html instead.
     favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
